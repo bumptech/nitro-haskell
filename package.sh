04eb86ae56a5e38c570f1eacc155f1eb62c64c23
erin <edahlgren@bu.mp>
2013-07-10 16:43:40 -0700

    fix the packaging script

--- package.sh
+++ package.sh
@@ -1,13 +1,12 @@
 #1/bin/bash
 
 mkdir nitro-haskell 2> /dev/null
-mkdir nitro-haskell/c-bits 2> /dev/null
 
-cp LICENSE Setup.hs nitro-haskell/
-cp -r c-bits nitro-haskell/
+cp -r include nitro-haskell/
+cp LICENSE Setup.hs configure nitro.buildinfo.in nitro-haskell/
 cp -r dist/build/System nitro-haskell/
 
-sed 's/dist\/build\///g' nitro.cabal | sed '/c2hs/d' | sed '/extra-libraries/q' > nitro-haskell/nitro.cabal
+sed 's/dist\/build\///g' nitro.cabal | sed '/c2hs/d' | sed '/c-sources/q' > nitro-haskell/nitro.cabal
 
 pushd "nitro-haskell" > /dev/null
 cabal configure
